5314451ab36a9c8ff6d830960085c9e2f60c2071
Giorgi Okruadze <41172610+GiorgiOkruadze@users.noreply.github.com>
2022-02-09 23:50:06 +0400

    cloudinaryMediaService added

---
+++ src/NgGlobal.ApplicationServices/FileStorageService/CloudinaryMediaService.cs
@@ -0,0 +1,30 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NgGlobal.ApplicationServices.FileStorageService
+{
+    public class CloudinaryMediaService
+    {
+        public string ApiKey { get; set; } = "917751897546634";
+        public string ApiSecret { get; set; } = "zJjTgbtW6whsT-feRg5F-LGs-KM";
+        public string Cloud { get; set; } = "itacademystep-develoeprs";
+
+
+        public string UploadFile(string filePath)
+        {
+            var myAccount = new Account { ApiKey = ApiKey, ApiSecret = ApiSecret, Cloud = Cloud };
+            Cloudinary _cloudinary = new(myAccount);
+            var uploadParams = new ImageUploadParams()
+            {
+                File = new FileDescription(filePath)
+            };
+            var uploadResult = _cloudinary.Upload(uploadParams);
+            return uploadResult.Url.AbsoluteUri;
+        }
+    }
+}

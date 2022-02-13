08445fb553778f0a132347bfc49bec2dfa601b77
Giorgi Okruadze <41172610+GiorgiOkruadze@users.noreply.github.com>
2022-02-13 14:50:28 +0400

    LogIn and Registration added

---
+++ src/NgGlobal.ApplicationServices/Handlers/User/LogInUserHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using NgGlobal.ApplicationServices.Authentication.Abstraction;
+using NgGlobal.ApplicationServices.Commands;
+using NgGlobal.CoreServices.Repositories.Abstractions;
+using NgGlobal.DatabaseModels.Models;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NgGlobal.ApplicationServices.Handlers
+{
+    public class LogInUserHandler : IRequestHandler<LogInUserCommand, string>
+    {
+        private readonly IUserRepository _userRepository = default;
+        private readonly IJwtAuthenticationManager _authenticationManager = default;
+
+        public LogInUserHandler(IUserRepository userRepository, IJwtAuthenticationManager authenticationManager)
+        {
+            _userRepository = userRepository;
+            _authenticationManager = authenticationManager;
+        }
+
+        public async Task<string> Handle(LogInUserCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _userRepository.LogInAsync(request.Email,request.Password);
+            return _authenticationManager.Authenticate(result, request.Email);
+        }
+    }
+}
